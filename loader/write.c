9b9bbdc1d0aa30fa3605c6e51575e2f4d3665047
Guillaume Charvolin <guillaume.charvolin@epita.fr>
2022-03-24 19:20:38 +0100

    feat: advance a little bit the write

--- loader/write.c
+++ loader/write.c
@@ -9,7 +9,23 @@ void write_habitant_number(int habitant_number)
 	int habitant_number;
 	while(!isHabitant(buffer))
 		fgets(buffer, LINE_BUFFER_SIZE, file);
-	write_value(i, , buffer);
+	write_value(habitant_number, 0, file);
 	fclose(file);
 }
 
+int digit(int value)
+{
+	for (int i = 0; i
+}
+
+int write_value(int value, char end, file)
+{
+	while (*building_line != '[')
+        ++building_line;
+	++building_line;
+
+	char buff[32];
+	sprintf(buff, "%d", value);
+	
+	ssize_t x = write(file, buffer, 
+}
